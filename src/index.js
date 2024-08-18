ac5c5f83e032d97be5aab76975f072cfa55f382b
Shivam Anand <shivam850anand@gmail.com>
2024-08-18 20:39:15 +0530

    stateModel addded t codebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { app } from "./app.js";
 import { appconfig } from "./config/appconfig.js";
+import { stateData } from "./data/index.js";
 import { connectdb } from "./database/dbconnect.js";
+import StateModel from "./models/stateModel.js";
 
 (async () => {
   try {
@@ -17,6 +19,7 @@ import { connectdb } from "./database/dbconnect.js";
         `Server started at http://localhost:${appconfig.PORT || 3030}/`
       );
     });
+
   } catch (error) {
     console.log(error);
   }
